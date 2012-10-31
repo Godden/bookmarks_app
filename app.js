332c308f1c74b61e4ad89794b69530ff9394dac7
Peter Godden <pgodden@zendesk.com>
2012-10-31 16:48:43 +1100

    Back to basics :-)

--- app.js
+++ app.js
@@ -1,119 +1,48 @@
 (function() {
 
-  var STATUS_CLOSED = 'closed';
-
   return {
+    
     defaultState: 'loading',
 
     requests: {
-      fetchBookmarks: {
-        url: '/api/v1/bookmarks.json'
-      },
 
-      addBookmark: function() {
-        return {
-          url: '/api/v1/bookmarks.json',
-          type: 'POST',
-          data: {
-            ticket_id: this.ticket().id()
-          }
-        };
-      },
-
-      destroyBookmark: function(toDestroy) {
-        return {
-          url: helpers.fmt('/api/v1/bookmarks/%@.json', toDestroy),
-          type: 'POST',
-          data: { _method: 'DELETE' }
-        };
-      }
     },
 
     events: {
-      'app.activated': 'requestBookmarks',
-
-      'fetchBookmarks.done': function(data) {
-        this.renderBookmarks((data || {}).bookmarks);
-      },
-
-      'fetchBookmarks.fail': function(data) {
-        this.switchTo('fetch_fail');
-      },
-
-      'addBookmark.done': function() {
-        services.notify(this.I18n.t('add.done', { id: this.ticket().id() }));
-      },
-
-      'addBookmark.fail': function() {
-        services.notify(this.I18n.t('add.failed', { id: this.ticket().id() }), 'error');
-      },
-
-      'addBookmark.always': function() {
-        this.ajax('fetchBookmarks');
-      },
-
-      'click .bookmark': function(event) {
-        event.preventDefault();
-        this.ajax('addBookmark');
-      },
-
-      'click .destroy': 'destroyBookmark',
-
-      'click a[data-role="reload-bookmarks"]': 'requestBookmarks'
+      'app.activated': 'start'
     },
 
-    renderBookmarks: function(bookmarks) {
-      this.bookmarks = bookmarks;
+  
+    start: function() {
+      
+      alert('The app has activated');
+
+      this.displayNames();
+
+    },
+
+    displayNames: function(){
+      this.names = ["Stefan", "Jason", "Peter", "Matt", "Michael", "Lachlan"];
+      this.accounts = [
+        {
+          name: "Batman",
+          id: 23434123,
+          link: "http://www.google.com/"
+        },
+        {
+          name: "Robin",
+          id: 435435,
+          link: "http://zendesk.com/"
+        }
+      ];
+
       this.switchTo('list', {
-        bookmarks:            this.bookmarks,
-        ticketIsBookmarkable: this.ticketIsBookmarkable()
+        namesToDisplay: this.names,
+        accounts: this.accounts
       });
-    },
 
-    ticketIsBookmarkable: function() {
-      var status = this.ticket().status() || STATUS_CLOSED;
-      if ( status == STATUS_CLOSED ) { return false; }
-
-      var ticketID = this.ticket().id(),
-          alreadyBookmarked = _.any(this.bookmarks, function(b) {
-            return b.ticket.nice_id === ticketID;
-          });
-
-      return !alreadyBookmarked;
-    },
-
-    requestBookmarks: function() {
-      this.ajax('fetchBookmarks');
-    },
-
-    // Get the bookmark ID for a click event within a bookmark <li>
-    bookmarkID: function(event) {
-      return this.$(event.target)
-                 .closest('[data-bookmark-id]')
-                 .data('bookmark-id');
-    },
-
-    destroyBookmark: function(event) {
-      event.preventDefault();
-      var toDestroy = this.bookmarkID(event);
-      if (!toDestroy) {
-        return;
-      }
-
-      var self = this;
-      this.ajax('destroyBookmark', toDestroy).done(function() {
-        self.removeDestroyedBookmark(toDestroy);
-      }).fail(function() {
-        services.notify(self.I18n.t('destroy.failed'), 'error');
-      });
-    },
-
-    removeDestroyedBookmark: function(toDestroy) {
-      this.renderBookmarks(_.reject(this.bookmarks, function(b) {
-        return b.id === toDestroy;
-      }));
-      services.notify(this.I18n.t('destroy.done'));
     }
+
 
   };
 
